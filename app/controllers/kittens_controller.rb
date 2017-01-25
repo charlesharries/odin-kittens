812c2d2e93a3a8d276c7877bac516f11f2509b06
Charles Harries <charlesgharries@gmail.com>
2017-01-25 16:46:12 -0500

    Complete HTML functionality

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,51 @@
+class KittensController < ApplicationController
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Way to go champ now you have a new fucking kitten"
+      redirect_to @kitten
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update_attributes(kitten_params)
+      flash[:success] = "You got it sport, your kitten's all different now"
+      redirect_to @kitten
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id]).destroy
+    flash[:success] = "Kitten was destroyed, which is fine because it's an abstract fucking concept for the purposes of this exercise, fuck"
+    redirect_to root_url
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+
+end
---
+++ app/models/kitten.rb
@@ -0,0 +1,16 @@
+class Kitten < ApplicationRecord
+
+  validates :name, presence: true
+  validates :age,      presence: true,
+                       numericality: { only_integer: true,
+                                   less_than: 16 }
+  validates :cuteness, presence: true,
+                       numericality: { only_integer: true,
+                                       greater_than: 0,
+                                       less_than_or_equal_to: 10 }
+  validates :softness, presence: true,
+                       numericality: { only_integer: true,
+                                       greater_than: 0,
+                                       less_than_or_equal_to: 10 }
+
+end
